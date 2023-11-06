2223e4009229f1fc722ba96385c4007e310a1037
xSYl25 <142290884+xSyl25@users.noreply.github.com>
2023-11-05 21:50:46 -0600

    nest the code for the tile effects in a function

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ document.addEventListener("load", createGrid(defaultN));
 
 function createGrid(amount) {
     let wh = 580 / amount;
-    
+
     for(i = 0; i < (amount * amount); i++) {
     let tile = document.createElement("div");
     tile.classList.add("grid");
     tile.setAttribute("style", `width: ${wh}px; height: ${wh}px; background-color: #fff;`);
     container.appendChild(tile);
     }
+
+    hoverEffect();
 }
 
+function hoverEffect() {
 let tiles = document.querySelectorAll(".grid");
 
 tiles.forEach(tile => {
@@ -29,8 +32,9 @@ tiles.forEach(tile => {
     })
 });
 
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
+    function random(number) {
+        return Math.floor(Math.random() * (number + 1));
+    }
 }
 
 btn.addEventListener("click", reset);
